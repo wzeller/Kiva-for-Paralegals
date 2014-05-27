a90d9ab0f77a1afd58097f86702b46a686206e76
Bill Zeller <wzeller@gmail.com>
2014-05-27 11:46:44 -0700

    trying to get heroku to work with AWS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,14 +81,15 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.paperclip_defaults = {                                   
-  :storage => :s3,                   
-  :s3_host_name => 's3-us-west-1.amazonaws.com',
-  :s3_credentials => {                                          
-    :bucket => ENV['AWS_PROD_BUCKET'],                        
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],                 
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']          
-    },                                                             
+ config.paperclip_defaults = {                                   
+    :storage => :s3,
+    :s3_host_name => 's3-us-west-1.amazonaws.com',                                                                                                                   
+    :path => '/:class/:attachment/:id_partition/:style/:filename',
+    :s3_credentials => {                                          
+      :bucket => ENV['AWS_PROD_BUCKET'],             
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],                 
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']          
+      },                                                          
   }
 
 end
