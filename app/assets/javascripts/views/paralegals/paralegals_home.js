62bd01e5c9dcd49d8d0aae60404ca306f8d76625
Bill Zeller <wzeller@gmail.com>
2014-05-29 12:18:47 -0700

    modal partly working

--- app/assets/javascripts/views/paralegals/paralegals_home.js
+++ app/assets/javascripts/views/paralegals/paralegals_home.js
@@ -2,16 +2,43 @@ KivaClone.Views.ParalegalsHome = Backbone.View.extend({
   
   initialize: function(){
     this.listenTo(this.collection, "sync add remove", this.render);
+    this.fired = false;
   },
 
   events: {
-    // "mouseover .col-md-1": "handlePhotoClick"
+    "mouseover a": "displayModal",
+    "mouseleave #home-modal": "removeModal"
   }, 
 
   template: JST['paralegals/home'],
 
-  paralegalSubview: function(collection){
+  modalContent: JST['paralegals/modal'],
 
+  displayModal: function(event){
+    if (this.fired == false){
+      var paralegalURL = event.currentTarget.getAttribute("href");
+      var paralegalId = paralegalURL.replace( /^\D+/g, '');
+      var paralegal = KivaClone.Collections.paralegals.getOrFetch(paralegalId);
+      var modalContent = this.modalContent({paralegal: paralegal});
+      this.$el.append(modalContent);
+      var x = event.clientX 
+      var y = event.clientY 
+      var homeTop = $("#home-photos").offset().top
+      
+      // console.log(x);
+      // console.log(y);
+      // // debugger
+      if (y-50 < homeTop){y = homeTop} else {y = y-50}
+      $("#home-modal").css('position', 'absolute');
+      $("#home-modal").css('top', y); 
+      $("#home-modal").css('left', x-50);
+      this.fired = true;
+    }
+  },
+
+  removeModal: function(){
+    $('#home-modal').remove();
+    this.fired = false;
   },
 
   render: function(){
