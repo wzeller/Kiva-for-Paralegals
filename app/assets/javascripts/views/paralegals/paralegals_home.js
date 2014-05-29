8f31dd418f1022dfe97e7fd7530b011c95f228f8
Bill Zeller <wzeller@gmail.com>
2014-05-29 14:40:58 -0700

    front page modal finished

--- app/assets/javascripts/views/paralegals/paralegals_home.js
+++ app/assets/javascripts/views/paralegals/paralegals_home.js
@@ -6,7 +6,7 @@ KivaClone.Views.ParalegalsHome = Backbone.View.extend({
   },
 
   events: {
-    "mouseover a": "displayModal",
+    "mouseenter a": "displayModal",
     "mouseleave #home-modal": "removeModal"
   }, 
 
@@ -24,16 +24,26 @@ KivaClone.Views.ParalegalsHome = Backbone.View.extend({
       var x = event.clientX 
       var y = event.clientY 
       var homeTop = $("#home-photos").offset().top
-      
+      var homeBottom = $("#home-photos").height()+$("#home-photos").offset().top
+      var right = $(window).width()
       // console.log(x);
       // console.log(y);
-      // // debugger
-      if (y-50 < homeTop){y = homeTop} else {y = y-50}
+
+      if (y-100 < homeTop){y = homeTop} else {y = y-50}
+      if (y+170 > homeBottom){y = homeBottom-180}
+      if (x-100 < 0){x=0} else {x = x-100}
+      if (x+500 > right){x = right-540}
+
       $("#home-modal").css('position', 'absolute');
       $("#home-modal").css('top', y); 
-      $("#home-modal").css('left', x-50);
+      $("#home-modal").css('left', x);
       this.fired = true;
     }
+
+    // if (this.fired == true && event.currentTarget){
+    //   $('#home-modal').remove();
+    //   this.fired = false;
+    // }
   },
 
   removeModal: function(){
