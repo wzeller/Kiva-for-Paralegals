8396d66daa79b8e888d58d7c72b50b96878b414f
Bill Zeller <wzeller@gmail.com>
2014-05-30 15:50:42 -0700

    change redirect to team page; change team pics to max at 200

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -8,7 +8,7 @@
       @user = current_user
       @team = @user.teams.build(team_params)
       if @team.save
-        redirect_to "#/user"
+        redirect_to "#myTeams"
       else
         flash.now[:errors] = @team.errors.full_messages
         render :new
